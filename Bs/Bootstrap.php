faebe38b96cf9a7b56f6747d2bba3911d62f855a
mifsudm <mifsudm@unimelb.edu.au>
2018-07-25 10:54:38 +1000

    ~Auto: Commit

--- Bs/Bootstrap.php
+++ Bs/Bootstrap.php
@@ -8,7 +8,7 @@ use Monolog\Logger;
 /**
  * @author Michael Mifsud <info@tropotek.com>
  * @link http://www.tropotek.com/
- * @license Copyright 2015 Michael Mifsud
+ * @license Copyright 2018 Michael Mifsud
  */
 class Bootstrap
 {
