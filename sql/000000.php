8d846393b2e6c2219e96e4a81834ee665c1b3211
mifsudm <mifsudm@unimelb.edu.au>
2018-08-23 11:42:49 +1000

    ~Auto: Commit

--- sql/000000.php
+++ sql/000000.php
@@ -4,8 +4,10 @@
 $config = \App\Config::getInstance();
 try {
     $data = \Tk\Db\Data::create();
-    if (!$data->get('site.title'))
-        $data->set('site.title', 'New Project');
+    if (!$data->get('site.title')) {
+        $data->set('site.title', 'Tk II Project');
+        $data->set('site.short.title', 'Tk2Base');
+    }
     if (!$data->get('site.email'))
         $data->set('site.email', 'admin@example.com');
     //$data->set('site.client.registration', 'site.client.registration');
