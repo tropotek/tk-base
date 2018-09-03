b6699c83a7b8f2c3cbde2665187c159c254a6bdd
mifsudm <mifsudm@unimelb.edu.au>
2018-09-03 10:15:23 +1000

    ~Auto: Commit

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,13 +6,18 @@
 jQuery(function ($) {
 
   //
-  project_core.initDatetimePicker();
   project_core.initLinkBlur();
   project_core.initTkFormTabs();
-  project_core.initTkFileInput();
   project_core.initGrowLikeAlerts();
   project_core.initDataConfirm();
   project_core.initTkPanel();
+  project_core.initTableDeleteConfirm();
+  project_core.initMasqueradeConfirm();
+
+  // Form Field Scripts
+  project_core.initDatetimePicker();
+  project_core.initDualListBox();
+  project_core.initTkFileInput();
 
 });
 
