16bc032d56f7d2e61653519d95c5c8bb311cf6f0
godar <info@tropotek.com>
2018-09-16 04:36:00 +1000

    ~Auto: Commit

--- sql/mysql/000000.sql
+++ sql/mysql/000000.sql
@@ -2,15 +2,15 @@
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `email` varchar(255) NOT NULL DEFAULT '',
-  `username` varchar(255) NOT NULL DEFAULT '',
-  `password` varchar(255) NOT NULL DEFAULT '',
-  `role` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `email` varchar(128) NOT NULL DEFAULT '',
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL DEFAULT '',
+  `role` varchar(32) NOT NULL DEFAULT '',   -- deprecated
   `notes` TEXT,
   `last_login` TIMESTAMP,
   `active` TINYINT(1) NOT NULL DEFAULT 1,
-  `hash` varchar(255) NOT NULL DEFAULT '',
+  `hash` varchar(64) NOT NULL DEFAULT '',
   `del` TINYINT(1) NOT NULL DEFAULT 0,
   `modified` DATETIME NOT NULL,
   `created` DATETIME NOT NULL,
