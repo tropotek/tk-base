2c3fa03bc104f99bb8d3b945aba7adb63a7d9304
Tropotek <info@tropotek.com>
2018-11-17 08:41:38 +1100

    ~Auto: Commit

--- assets/js/elFinder/php/connector.minimal.php
+++ assets/js/elFinder/php/connector.minimal.php
@@ -134,11 +134,14 @@ $opts = array(
 			//'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
 			'trashHash'     => 't1_Lw',                         // elFinder's hash of trash folder
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/',     // to make hash same to Linux one on windows too
-			'uploadDeny'    => array('all'),                    // All Mimetypes not allowed to upload
-			'uploadAllow'   => array('image', 'video', 'audio', 'text', 'model', 'font',
+            'accessControl' => 'access',                         // disable and hide dot starting files (OPTIONAL)
+
+            'uploadDeny'    => array('all'),
+            //'uploadAllow'   => array('image', 'text/plain'),
+            'uploadAllow'   => array('image', 'video', 'audio', 'text/plain', 'model', 'font', 'application',
                 'application/pdf', 'application/vnd.ms-excel', 'application/vnd.ms-powerpoint', 'application/msword', 'application/vnd.ms-word'),    // Mimetype `image` and `text/plain` allowed to upload
-			'uploadOrder'   => array('deny', 'allow'),          // allowed Mimetype `image` and `text/plain` only
-			'accessControl' => 'access'                         // disable and hide dot starting files (OPTIONAL)
+            'uploadOrder'   => array('deny', 'allow'),
+
 		),
 		// Trash volume
 		array(
@@ -149,11 +152,14 @@ $opts = array(
 			//'path'          => '../files/.trash/',
 			//'tmbURL'        => dirname($_SERVER['PHP_SELF']) . '/../files/.trash/.tmb/',
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/',     // to make hash same to Linux one on windows too
-			'uploadDeny'    => array('all'),                    // Recomend the same settings as the original volume that uses the trash
-            'uploadAllow'   => array('image', 'video', 'audio', 'text', 'model', 'font',
+            'accessControl' => 'access',                        // Same as above
+
+            'uploadDeny'    => array('all'),
+            //'uploadAllow'   => array('image', 'text/plain'),
+            'uploadAllow'   => array('image', 'video', 'audio', 'text/plain', 'model', 'font', 'application',
                 'application/pdf', 'application/vnd.ms-excel', 'application/vnd.ms-powerpoint', 'application/msword', 'application/vnd.ms-word'),    // Mimetype `image` and `text/plain` allowed to upload
-			'uploadOrder'   => array('deny', 'allow'),          // Same as above
-			'accessControl' => 'access',                        // Same as above
+            'uploadOrder'   => array('deny', 'allow'),
+            
 		)
 	)
 );
