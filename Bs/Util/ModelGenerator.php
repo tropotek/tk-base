1f03884c3cad4a7a4cc82fe4f864ebe72c45ffdd
mifsudm <mifsudm@unimelb.edu.au>
2019-06-11 14:15:17 +1000

    ~Auto: Commit

--- Bs/Util/ModelGenerator.php
+++ Bs/Util/ModelGenerator.php
@@ -340,7 +340,7 @@ class {classname}Map extends Mapper
      */
     public function findFiltered(\$filter, \$tool = null)
     {
-        \$filter = \Tk\Db\Filter::create(\$filter, \$tool);
+        \$filter = \Tk\Db\Filter::create(\$filter);
         \$this->makeQuery(\$filter);
         \$res = \$this->selectFrom(\$filter->getFrom(), rtrim(\$filter->getWhere(), 'AND '), \$tool);
         return \$res;
