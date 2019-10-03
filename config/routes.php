5ce074e64e4a97f3d1b0c9ed7aa0662cd376162f
mifsudm <mifsudm@unimelb.edu.au>
2019-10-03 12:01:39 +1000

    Update Tk\Routing\Route constructor to be compatable with sysmfony Route object

--- config/routes.php
+++ config/routes.php
@@ -10,6 +10,7 @@
  *     array('GET', 'POST', 'HEAD')     // methods
  * );
  */
+use Tk\Routing\Route;
 
 $config = \Bs\Config::getInstance();
 $routes = $config->getRouteCollection();
@@ -17,54 +18,54 @@ if (!$routes) return;
 
 
 // Used to redirect index.php request back home in rarer instances
-$routes->add('public-index-php-fix', new \Tk\Routing\Route('/index.php', function ($request) {
+$routes->add('public-index-php-fix', Route::create('/index.php', function ($request) {
     \Tk\Uri::create('/')->redirect();
 }));
-$routes->add('home', new \Tk\Routing\Route('/index.html', 'Bs\Controller\Index::doDefault'));
-$routes->add('home-base', new \Tk\Routing\Route('/', 'Bs\Controller\Index::doDefault'));
+$routes->add('home', Route::create('/index.html', 'Bs\Controller\Index::doDefault'));
+$routes->add('home-base', Route::create('/', 'Bs\Controller\Index::doDefault'));
 
-$routes->add('login', new \Tk\Routing\Route('/login.html', 'Bs\Controller\Login::doDefault'));
-$routes->add('register', new \Tk\Routing\Route('/register.html', 'Bs\Controller\Register::doDefault'));
-$routes->add('recover', new \Tk\Routing\Route('/recover.html', 'Bs\Controller\Recover::doDefault'));
-$routes->add('logout', new \Tk\Routing\Route('/logout.html', 'Bs\Controller\Logout::doDefault'));
+$routes->add('login', Route::create('/login.html', 'Bs\Controller\Login::doDefault'));
+$routes->add('register', Route::create('/register.html', 'Bs\Controller\Register::doDefault'));
+$routes->add('recover', Route::create('/recover.html', 'Bs\Controller\Recover::doDefault'));
+$routes->add('logout', Route::create('/logout.html', 'Bs\Controller\Logout::doDefault'));
 
-$routes->add('maintenance', new \Tk\Routing\Route('/maintenance.html', 'Bs\Controller\Maintenance::doDefault'));
+$routes->add('maintenance', Route::create('/maintenance.html', 'Bs\Controller\Maintenance::doDefault'));
 
 
 // Admin Pages
-//$routes->add('admin-dashboard', new \Tk\Routing\Route('/admin/index.html', 'Bs\Controller\Admin\Dashboard::doDefault'));
-//$routes->add('admin-dashboard-base', new \Tk\Routing\Route('/admin/', 'Bs\Controller\Admin\Dashboard::doDefault'));
-$routes->add('admin-user-profile', new \Tk\Routing\Route('/admin/profile.html', 'Bs\Controller\Admin\User\Profile::doDefault'));
+//$routes->add('admin-dashboard', Route::create('/admin/index.html', 'Bs\Controller\Admin\Dashboard::doDefault'));
+//$routes->add('admin-dashboard-base', Route::create('/admin/', 'Bs\Controller\Admin\Dashboard::doDefault'));
+$routes->add('admin-user-profile', Route::create('/admin/profile.html', 'Bs\Controller\Admin\User\Profile::doDefault'));
 
-$routes->add('admin-role-manager', new \Tk\Routing\Route('/admin/roleManager.html', 'Bs\Controller\Role\Manager::doDefault'));
-$routes->add('admin-role-edit', new \Tk\Routing\Route('/admin/roleEdit.html', 'Bs\Controller\Role\Edit::doDefault'));
+$routes->add('admin-role-manager', Route::create('/admin/roleManager.html', 'Bs\Controller\Role\Manager::doDefault'));
+$routes->add('admin-role-edit', Route::create('/admin/roleEdit.html', 'Bs\Controller\Role\Edit::doDefault'));
 
-$routes->add('admin-user-manager', new \Tk\Routing\Route('/admin/userManager.html', 'Bs\Controller\Admin\User\Manager::doDefault'));
-$routes->add('admin-user-edit', new \Tk\Routing\Route('/admin/userEdit.html', 'Bs\Controller\Admin\User\Edit::doDefault'));
+$routes->add('admin-user-manager', Route::create('/admin/userManager.html', 'Bs\Controller\Admin\User\Manager::doDefault'));
+$routes->add('admin-user-edit', Route::create('/admin/userEdit.html', 'Bs\Controller\Admin\User\Edit::doDefault'));
 
-$routes->add('admin-settings', new \Tk\Routing\Route('/admin/settings.html', 'Bs\Controller\Admin\Settings::doDefault'));
-$routes->add('admin-plugin-manager', new \Tk\Routing\Route('/admin/plugins.html', 'Bs\Controller\Admin\PluginManager::doDefault'));
+$routes->add('admin-settings', Route::create('/admin/settings.html', 'Bs\Controller\Admin\Settings::doDefault'));
+$routes->add('admin-plugin-manager', Route::create('/admin/plugins.html', 'Bs\Controller\Admin\PluginManager::doDefault'));
 
 
 // User Pages
-//$routes->add('user-dashboard', new \Tk\Routing\Route('/user/index.html', 'Bs\Controller\User\Dashboard::doDefault'));
-//$routes->add('user-dashboard-base', new \Tk\Routing\Route('/user/', 'Bs\Controller\User\Dashboard::doDefault'));
-$routes->add('user-profile', new \Tk\Routing\Route('/user/profile.html', 'Bs\Controller\Admin\User\Profile::doDefault'));
+//$routes->add('user-dashboard', Route::create('/user/index.html', 'Bs\Controller\User\Dashboard::doDefault'));
+//$routes->add('user-dashboard-base', Route::create('/user/', 'Bs\Controller\User\Dashboard::doDefault'));
+$routes->add('user-profile', Route::create('/user/profile.html', 'Bs\Controller\Admin\User\Profile::doDefault'));
 
 
 // Admin Dev Pages
 $routes->add('admin-dev-events',
-    new \Tk\Routing\Route('/admin/dev/dispatcherEvents.html', 'Bs\Controller\Admin\Dev\Events::doDefault'));
-$routes->add('admin-tail-log', new \Tk\Routing\Route('/admin/dev/tailLog.html', 'Bs\Controller\Admin\Dev\Tail\Log::doDefault'));
+    Route::create('/admin/dev/dispatcherEvents.html', 'Bs\Controller\Admin\Dev\Events::doDefault'));
+$routes->add('admin-tail-log', Route::create('/admin/dev/tailLog.html', 'Bs\Controller\Admin\Dev\Tail\Log::doDefault'));
 
 
 // Examples
 
 // Ajax Routes
-//$routes->add('ajax-find-user', new \Tk\Routing\Route('/api/1.0/findUser', 'App\Ajax\User::doFindUser', array('POST')));
+//$routes->add('ajax-find-user', Route::create('/api/1.0/findUser', 'App\Ajax\User::doFindUser', array('POST')));
 
 // Example: How to do a simple controller/route all-in-one
-//$routes->add('simpleTest', new \Tk\Routing\Route('/test.html', function ($request) use ($config) {
+//$routes->add('simpleTest', Route::create('/test.html', function ($request) use ($config) {
 //    vd($config->toString());
 //    return '<p>This is a simple test</p>';
 //}));
