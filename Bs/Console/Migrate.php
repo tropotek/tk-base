d9bf8a56918a4bede8d7419132df4cee163d4e38
mifsudm <mifsudm@unimelb.edu.au>
2020-02-13 06:56:58 +1100

    ~Auto: Commit

---
+++ Bs/Console/Migrate.php
@@ -0,0 +1,44 @@
+<?php
+namespace Bs\Console;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * @author Michael Mifsud <info@tropotek.com>
+ * @see http://www.tropotek.com/
+ * @license Copyright 2017 Michael Mifsud
+ */
+class Migrate extends Iface
+{
+
+    /**
+     *
+     */
+    protected function configure()
+    {
+        $this->setName('migrate')
+            ->setAliases(array('mig'))
+            ->setDescription('Migrate the DB file for this project and its dependencies');
+        parent::configure();
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     * @throws \Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        parent::execute($input, $output);
+
+
+
+
+
+    }
+
+}
