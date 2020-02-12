af8a2d806362523ba5533f1c13370d29329bc6c3
mifsudm <mifsudm@unimelb.edu.au>
2020-02-13 09:00:46 +1100

    ~Auto: Commit

--- sql/mysql/000003.sql
+++ sql/mysql/000003.sql
@@ -14,8 +14,11 @@ alter table user
     add name_last varchar(128) default '' not null after name_first;
 
 -- Fill in the name fields
-UPDATE user SET name_first = SUBSTRING(name, 1, LOCATE(' ', name) - 1);
-UPDATE user SET name_last = SUBSTRING(name, LOCATE(' ', name) + 1);
+UPDATE user SET name_first = SUBSTRING(name, 1, LOCATE(' ', name) - 1) WHERE TRIM(name) != '';
+UPDATE user SET name_last = SUBSTRING(name, LOCATE(' ', name) + 1) WHERE TRIM(name) != '';
+
+UPDATE user SET name_first = username WHERE TRIM(name_first) = '';
+
 
 
 
