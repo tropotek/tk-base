b3fb88e486bfdebafe83525ae56279d6f27f12e0
tropotek <info@tropotek.com>
2021-04-21 08:01:09 +1000

    ~Auto: Commit

---
+++ Bs/Db/FileIface.php
@@ -0,0 +1,25 @@
+<?php
+namespace Bs\Db;
+
+use Tk\Db\Tool;
+
+/**
+ * Use this interface if you are planing on using the file upload field
+ *
+ *
+ *
+ * @author Michael Mifsud <info@tropotek.com>
+ * @link http://www.tropotek.com/
+ * @license Copyright 2018 Michael Mifsud
+ */
+interface FileIface
+{
+
+    /**
+     * @param Tool $tool
+     * @return array|File[]
+     */
+    public function getFileList(Tool $tool);
+
+
+}
