e4942723bf022d8289a795dbc1398f5464ea002e
tropotek <info@tropotek.com>
2022-08-09 08:19:01 +1000

    ~Auto: Commit

--- sql/mysql/000006.sql
+++ sql/mysql/000006.sql
@@ -3,8 +3,10 @@
 --
 -- @author: Michael Mifsud <info@tropotek.com>
 -- --------------------------------------------
-
-SELECT count(*) into @colCnt FROM information_schema.columns WHERE table_name = 'file' AND column_name = 'selected' and table_schema = DATABASE();
-IF @colCnt = 0 THEN
-    ALTER TABLE `file` ADD COLUMN `selected` BOOL NOT NULL DEFAULT FALSE AFTER `notes`;
+SELECT count(*) into @tblCnt FROM information_schema.TABLES WHERE TABLE_NAME = 'file' AND TABLE_SCHEMA in (SELECT DATABASE());
+IF @tblCnt > 0 THEN
+  SELECT count(*) into @colCnt FROM information_schema.columns WHERE table_name = 'file' AND column_name = 'selected' and table_schema = DATABASE();
+  IF @colCnt = 0 THEN
+      ALTER TABLE `file` ADD COLUMN `selected` BOOL NOT NULL DEFAULT FALSE AFTER `notes`;
+  END IF;
 END IF;
