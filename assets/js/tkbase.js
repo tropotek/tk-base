58ee4548f72a42ec69138ff52cb0e49cd84576cd
tropotek <info@tropotek.com>
2023-06-21 16:19:36 +1000

    ~Auto: Commit

--- assets/js/tkbase.js
+++ assets/js/tkbase.js
@@ -81,6 +81,34 @@ function vd() {
   for(let k in arguments) console.log(arguments[k]);
 }
 
+function copyToClipboard(text) {
+  if(navigator.clipboard) {
+    // Modern versions of Chromium browsers, Firefox, etc.
+    navigator.clipboard.writeText(text);
+  } else if (window.clipboardData) {
+    // Internet Explorer.
+    window.clipboardData.setData('Text', text);
+  } else {
+    // Fallback method using Textarea.
+    var textArea = document.createElement('textarea');
+    textArea.value          = text;
+    textArea.style.position = 'fixed';
+    textArea.style.top      = '-999999px';
+    textArea.style.left     = '-999999px';
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      if (!document.execCommand('copy')) {
+        console.warn('Could not copy text to clipboard');
+      }
+    } catch (error) {
+      console.warn('Could not copy text to clipboard');
+    }
+    document.body.removeChild(textArea);
+  }
+}
+
 function clearForm(form) {
   $(':input', form).each(function () {
     var type = this.type;
