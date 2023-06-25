6721e5a31e3b98ad3eda2aad0d967bf4df7a9f01
tropotek <info@tropotek.com>
2023-06-25 13:37:19 +1000

    ~Auto: Commit

--- config/20-config.php
+++ config/20-config.php
@@ -10,8 +10,4 @@ return function (Config $config)
     $config->set('path.template.maintenance', '/html/public.html');
     $config->set('path.template.login',       '/html/login.html');
 
-    $config->set('sql.migrate.list', [
-        'App Sql' => $config->getBasePath() . '/src/config',
-        'Bs Sql'  => $config->getBasePath() . $config->get('path.vendor.org') . '/tk-base/config/sql',
-    ]);
 };
