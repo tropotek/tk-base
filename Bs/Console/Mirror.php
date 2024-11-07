08c0e3baabf54aa3734bfc1655e54d1a9d8cafd8
tropotek <info@tropotek.com>
2024-11-08 10:43:01 +1100

    ~Auto: Commit

--- Bs/Console/Mirror.php
+++ Bs/Console/Mirror.php
@@ -131,7 +131,7 @@ class Mirror extends Console
     protected function postRequest(Uri|string $srcUrl, string $filename): bool
     {
         $ok     = true;
-        $srcUrl = Uri::create($srcUrl)->setScheme(Uri::SCHEME_HTTP_SSL);
+        $srcUrl = Uri::create($srcUrl)->withScheme('https');
         $query  = $srcUrl->getQuery();
         $srcUrl->reset();
         $secret = $this->getConfig()->get('db.mirror.secret', '');
