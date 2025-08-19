6bc99d1dbd41c81eef48074ed0fee43428694d31
tropotek <info@tropotek.com>
2025-08-19 14:05:30 +1000

    ~Auto: Commit

--- Bs/Controller/Util/DbSize.php
+++ Bs/Controller/Util/DbSize.php
@@ -54,13 +54,13 @@ class DbSize extends ControllerAdmin
 
         $this->table->appendCell('table_rows')
             ->setHeader('Row Count')
-            ->addHeaderCss('text-right')
-            ->addCss('text-nowrap text-right');
+            ->addHeaderCss('text-end')
+            ->addCss('text-nowrap text-end');
 
         $this->table->appendCell('size_bytes')
             ->setHeader('Size')
-            ->addHeaderCss('text-right')
-            ->addCss('text-nowrap text-right')
+            ->addHeaderCss('text-end')
+            ->addCss('text-nowrap text-end')
             ->addOnValue(function(\stdClass $obj, Cell $cell) {
                 if ($obj->table_name == 'Total') {
                     $cell->getTable()->getRowAttrs()->setAttr('class', 'text-strong bg-secondary-subtle');
